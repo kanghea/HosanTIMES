b817f41119bed6b72b1c6619fcf9e78c8f153f61
kanghea <kanghea26@gmail.com>
2022-08-03 10:42:19 +0900

    ha

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Main from './main';
-import Map from './map';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Student from './student';
+import Teacher from './teacher';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,6 +13,7 @@ root.render(
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/student" element={<Student/>} />
+      <Route path="/teacher" element={<Teacher/>} />
     </Routes>
   </Router>
 );
