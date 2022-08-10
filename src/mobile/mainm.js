449a74dea51cfa9a9933bca7eb49b19a2f47246b
kanghea <kanghea26@gmail.com>
2022-08-10 11:58:53 +0900

    1123

--- src/mobile/mainm.js
+++ src/mobile/mainm.js
@@ -6,13 +6,13 @@ function MainM(){
             <div className='text-white'>
                 <img alt="logo" src="img/jongrologo.png" className='h-20 w-auto' />
                 <div className='text-center mt-3'>
-                    <div className='button'>
+                    <div className='p-2 bg-slate-900 border-2 border-white rounded-[40px] mt-2'>
                         학생모드
                     </div>
-                    <div className='button'>
+                    <div className='p-2 bg-slate-900 border-2 border-white rounded-[40px] mt-2'>
                         선생님모드
                     </div>
-                    <div className='button'>
+                    <div className='p-2 bg-slate-900 border-2 border-white rounded-[40px] mt-2'>
                         학부모모드  
                     </div>
                 </div>
