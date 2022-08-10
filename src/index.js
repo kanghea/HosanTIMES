9a71419dff1122b56eec3507ada8f5c3e0be18e0
kanghea <kanghea26@gmail.com>
2022-08-10 11:10:34 +0900

    src setting

--- src/index.js
+++ src/index.js
@@ -4,19 +4,26 @@ import './index.css';
 import Main from './main';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserView, MobileView } from 'react-device-detect'
 import Student from './student';
 import Teacher from './teacher';
 import Parents from './Parents';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/student" element={<Student/>} />
-      <Route path="/teacher" element={<Teacher/>} />
-      <Route path="/parents" element={<Parents/>} />
-    </Routes>
+    <BrowserView>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/student" element={<Student/>} />
+        <Route path="/teacher" element={<Teacher/>} />
+        <Route path="/parents" element={<Parents/>} />
+      </Routes>
+    </BrowserView>
+    <MobileView>
+      
+    </MobileView>
   </Router>
 );
 
